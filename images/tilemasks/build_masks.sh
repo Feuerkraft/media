545cc653a7614ed02f68a230f53bc93c11ac6d77
Ingo Ruhnke <grumbel@gmail.com>
2009-07-25 17:59:25 +0000

    Renamed masks

--- images/tilemasks/build_masks.sh
+++ images/tilemasks/build_masks.sh
@@ -5,30 +5,30 @@ BUILDDIR="masks"
 # flop == "horizontal flip"
 
 for i in bbwb*.png; do
-    cp "$i" "${BUILDDIR}/$i"
-    convert -flop "$i" "${BUILDDIR}/bbbw${i##bbwb}"
-    convert -flip "$i" "${BUILDDIR}/bwbb${i##bbwb}"
-    convert -flip -flop "$i" "${BUILDDIR}/wbbb${i##bbwb}"
+    convert -negate "$i" "${BUILDDIR}/$i"
+#    convert -flop "$i" "${BUILDDIR}/bbbw${i##bbwb}"
+#    convert -flip "$i" "${BUILDDIR}/bwbb${i##bbwb}"
+#    convert -flip -flop "$i" "${BUILDDIR}/wbbb${i##bbwb}"
 done
 
 for i in bwbw*.png; do
-    cp "$i" "${BUILDDIR}/$i"
-    convert -flip "$i" "${BUILDDIR}/wbwb${i##bwbw}"
+    convert -negate "$i" "${BUILDDIR}/$i"
+#    convert -flip "$i" "${BUILDDIR}/wbwb${i##bwbw}"
 done
 
 for i in bbww*.png; do
-    cp "$i" "${BUILDDIR}/$i"
-    convert -flip "$i" "${BUILDDIR}/wwbb${i##bbww}"
-    convert -transpose  "$i" "${BUILDDIR}/bwwb${i##bbww}"
-    convert -transverse "$i" "${BUILDDIR}/wbbw${i##bbww}"
+    convert -negate "$i" "${BUILDDIR}/$i"
+#    convert -flip "$i" "${BUILDDIR}/wwbb${i##bbww}"
+#    convert -transpose  "$i" "${BUILDDIR}/bwwb${i##bbww}"
+#    convert -transverse "$i" "${BUILDDIR}/wbbw${i##bbww}"
 done
 
 
 for i in bwww*.png; do
-    cp "$i" "${BUILDDIR}/$i"
-    convert -flip "$i" "${BUILDDIR}/wwwb${i##bwww}"
-    convert -flop "$i" "${BUILDDIR}/wbww${i##bwww}"
-    convert -transverse "$i" "${BUILDDIR}/wwbw${i##bwww}"
+    convert -negate "$i" "${BUILDDIR}/$i"
+#    convert -flip "$i" "${BUILDDIR}/wwwb${i##bwww}"
+#    convert -flop "$i" "${BUILDDIR}/wbww${i##bwww}"
+#    convert -transverse "$i" "${BUILDDIR}/wwbw${i##bwww}"
 done
 
 # EOF #
--- images/tilemasks/build_tiles.sh
+++ images/tilemasks/build_tiles.sh
@@ -5,10 +5,10 @@ mkdir -p tiles
 for i in masks/*.png; do
     echo $i
     NAME=$(basename $i)
-    ./apply_alpha.py grass.png "$i" "tiles/grass_${NAME}"
-    ./apply_alpha.py mud.png   "$i" "tiles/mud_${NAME}"
-    ./apply_alpha.py darksand.png "$i" "tiles/darksand_${NAME}"
-    # ./apply_alpha.py sand.png  "$i" "tiles/sand_${NAME}"
+    NAME=${NAME%%.png}
+    ./apply_alpha.py grass.png    "$i" "tiles/$(echo "$NAME" | tr [bw] [g_]).png"
+    ./apply_alpha.py mud.png      "$i" "tiles/$(echo "$NAME" | tr [bw] [m_]).png"
+    ./apply_alpha.py darksand.png "$i" "tiles/$(echo "$NAME" | tr [bw] [d_]).png"
 done
 
 # EOF #
