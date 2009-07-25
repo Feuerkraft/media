3c0dcd94ed89e42f0ae18aa76834ca0fbe71cc44
Ingo Ruhnke <grumbel@gmail.com>
2009-07-25 18:55:22 +0000

    Force grayscale in mask generation

--- images/tilemasks/build_masks.sh
+++ images/tilemasks/build_masks.sh
@@ -4,6 +4,8 @@ BUILDDIR="masks"
 
 mkdir -p "${BUILDDIR}"
 
+IMAGICKFLAGS="-type Grayscale"
+
 # flop == "horizontal flip"
 
 # upper/left
@@ -12,34 +14,34 @@ for i in 1000-*.png; do
     EXT="${i##1000-}"
 
     # single spot
-    convert -negate "$i" "${BUILDDIR}/1000-${EXT}"
-    convert -flop "$i" "${BUILDDIR}/0100-${EXT}"
-    convert -flip "$i" "${BUILDDIR}/0001-${EXT}"
-    convert -flip -flop "$i" "${BUILDDIR}/0010-${EXT}"
+    convert $IMAGICKFLAGS -negate     "$i" "${BUILDDIR}/1000-${EXT}"
+    convert $IMAGICKFLAGS -flop       "$i" "${BUILDDIR}/0100-${EXT}"
+    convert $IMAGICKFLAGS -flip       "$i" "${BUILDDIR}/0001-${EXT}"
+    convert $IMAGICKFLAGS -flip -flop "$i" "${BUILDDIR}/0010-${EXT}"
 
     # three spot
-    convert -negate "$i" "${BUILDDIR}/0111-${EXT}"
-    convert -negate -flop "$i" "${BUILDDIR}/1011-${EXT}"
-    convert -negate -flip "$i" "${BUILDDIR}/1110-${EXT}"
-    convert -negate -flip -flop "$i" "${BUILDDIR}/1101-${EXT}"
+    convert $IMAGICKFLAGS -negate             "$i" "${BUILDDIR}/0111-${EXT}"
+    convert $IMAGICKFLAGS -negate -flop       "$i" "${BUILDDIR}/1011-${EXT}"
+    convert $IMAGICKFLAGS -negate -flip       "$i" "${BUILDDIR}/1110-${EXT}"
+    convert $IMAGICKFLAGS -negate -flip -flop "$i" "${BUILDDIR}/1101-${EXT}"
 done
 
 # diagonal
 for i in 1010-*.png; do
     echo $i
     EXT="${i##1010-}"
-    convert         "$i" "${BUILDDIR}/1010-${EXT}"
-    convert -negate "$i" "${BUILDDIR}/0101-${EXT}"
+    convert $IMAGICKFLAGS         "$i" "${BUILDDIR}/1010-${EXT}"
+    convert $IMAGICKFLAGS -negate "$i" "${BUILDDIR}/0101-${EXT}"
 done
 
 # one side
 for i in 1100-*.png; do
     echo $i
     EXT="${i##1100-}"
-    convert             "$i" "${BUILDDIR}/1010-${EXT}"
-    convert -flip       "$i" "${BUILDDIR}/0011-${EXT}"
-    convert -transpose  "$i" "${BUILDDIR}/1010-${EXT}"
-    convert -transverse "$i" "${BUILDDIR}/0101-${EXT}"
+    convert $IMAGICKFLAGS             "$i" "${BUILDDIR}/1010-${EXT}"
+    convert $IMAGICKFLAGS -flip       "$i" "${BUILDDIR}/0011-${EXT}"
+    convert $IMAGICKFLAGS -transpose  "$i" "${BUILDDIR}/1010-${EXT}"
+    convert $IMAGICKFLAGS -transverse "$i" "${BUILDDIR}/0101-${EXT}"
 done
 
 # EOF #
