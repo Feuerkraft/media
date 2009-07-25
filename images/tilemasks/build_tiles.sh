3f3a87994ef41aa78ade9e43c52cbcfb83442c46
Ingo Ruhnke <grumbel@gmail.com>
2009-07-25 19:12:06 +0000

    Added water to tile buildscript

--- images/tilemasks/build_tiles.sh
+++ images/tilemasks/build_tiles.sh
@@ -2,14 +2,25 @@
 
 mkdir -p tiles
 
+# a=asphalt
+# d=darkwater
+# w=darksand/wetsand
+# g=grass
+# m=mud
+# f=water
+# s=sand
+# i=snow
+
 for i in masks/*.png; do
     echo $i
     NAME=$(basename $i)
     NUM="${NAME:5:1}"
     NAME="${NAME:0:4}"
-    ./apply_alpha.py grass.png    "$i" "tiles/$(echo "$NAME" | tr [10] [g_])-${NUM}.png"
-    ./apply_alpha.py mud.png      "$i" "tiles/$(echo "$NAME" | tr [10] [m_])-${NUM}.png"
-    ./apply_alpha.py darksand.png "$i" "tiles/$(echo "$NAME" | tr [10] [d_])-${NUM}.png"
+    ./apply_alpha.py grass.png      "$i" "tiles/$(echo "$NAME" | tr [10] [g_])-${NUM}.png"
+    ./apply_alpha.py mud.png        "$i" "tiles/$(echo "$NAME" | tr [10] [m_])-${NUM}.png"
+    ./apply_alpha.py darksand.png   "$i" "tiles/$(echo "$NAME" | tr [10] [w_])-${NUM}.png"
+    ./apply_alpha.py water.png      "$i" "tiles/$(echo "$NAME" | tr [10] [f_])-${NUM}.png"
+    ./apply_alpha.py darkwater.png  "$i" "tiles/$(echo "$NAME" | tr [10] [d_])-${NUM}.png"
 done
 
 # EOF #
